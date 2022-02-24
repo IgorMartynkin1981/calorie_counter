bb255fb0b25dd61c6c7ba0b18f406fadfe9350b5
IgorMartynkin <imartynkin@yandex.ru>
2022-02-24 23:49:29 +0900

    Осталось доделать вывод информации

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,28 +1,62 @@
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StepTracker {
-    HashMap<Integer, MonthData> monthToData = new HashMap<Integer, MonthData>();
-    static int targetSteps = 10000;
+    static HashMap<Integer, int[]> monthToData = new HashMap<>();
+    //так как число дней в месяце закреплено  и = 30, то используем массив
+
+    static int targetSteps = 10000; // значение может изменяться из changeTargetSteps
+
+    // создадим масив с именами месяцев, для вывода на печать и возможно ещё где понадобится
     static String[] monthName = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
-    public StepTracker() {
-        for (int i = 0; i < 12; i++) {
-            monthToData.put(i, new MonthData());
+
+
+    //--------------------------------------------------------//
+    //Создаём обработу добавления значения
+    public static void StepTracker(int toMonthName, int toMonthData, int toSteps) {
+
+        if (!monthToData.containsKey(toMonthName)){ //if not found value
+            int[] monthData = new int[30];
+            monthData[(toMonthData - 1)] = toSteps;
+            monthToData.put(toMonthName, monthData);
+        } else {                                    //if found value
+            int[] monthData = monthToData.get(toMonthName);
+            monthData[(toMonthData - 1)] = toSteps;
+            //monthToData.put(toMonthName, monthData);
+
         }
     }
 
-    class MonthData {
-        // Заполните класс самостоятельно
+    //--------------------------------------------------------//
+    //Печать всей ХЭШ-таблицы. Для контроля при написании
+    static void printAllmonthToData() {
+        for (int category : monthToData.keySet()) {
+            int[] monthData = monthToData.get(category);
+            for (int i = 0; i < monthData.length; i++) {
+                System.out.println("Month: " + monthName[category-1] + " Day: " + (i+1) + " Step = " + monthData[i]);
+            }
+        }
     }
 
+    //--------------------------------------------------------//
+    /* changeTargetSteps присваивает новое значение цели по количеству шагов в день
+    значение newTargetStep прилетает из calorie_counter
+     */
     static void changeTargetSteps(int newTargetStep){
         targetSteps = newTargetStep;
         System.out.println(targetSteps);
     }
 
+    //для отображения месяцев при запросе о вводе и выводе
     static void printMonth(){
+        /*
         for (int i = 0; i < 12; i++) {
             System.out.println((i+1) + "-" + monthName[i]);
+        }
+        */
+        for (int i = 0; i < 12; i++) {
+            System.out.print((i+1) + "-" + monthName[i] + ": ");
         }
     }
 
--- src/calorie_counter.java
+++ src/calorie_counter.java
@@ -4,16 +4,47 @@ public class calorie_counter {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //int targetSteps = 10000;
 
         while (true) {
             printMenu(); //реализовываем печать меню
             int command = scanner.nextInt(); // повторное считывание данных от пользователя
+            //Ввести количество шагов за определённый день
             if (command == 1) {
-                System.out.println("Command 1");
+                System.out.println("Вы хотите ввести данные о пройденных шагах:");
                 StepTracker.printMonth();
+                System.out.println("");
+                int monthData;
+                int monthName;
+                while (true) {
+                    System.out.print("Введите месяц: ");
+                    int toMonthName = scanner.nextInt();
+                    if ((toMonthName < 1) || (toMonthName > 12)) {
+                        System.out.println("Число должно быть в диапозоне от 1 до 12");
+                    } else {
+                        monthName = toMonthName;
+                        break;
+                    }
+                }
+                while (true) {
+                    System.out.print("Введите число месяца (от 1 до 30): ");
+                    int toMonthData = scanner.nextInt();
+                    if ((toMonthData < 1) || (toMonthData > 30)) {
+                        System.out.println("Число должно быть в диапозоне от 1 до 30");
+                    } else {
+                        monthData = toMonthData;
+                        break;
+                    }
+                }
+                System.out.print("Введите количество пройденных шагов: ");
+                int toSteps = scanner.nextInt();
+                StepTracker.StepTracker(monthName, monthData, toSteps);
+                System.out.println("Информация сохранена!");
+
+            //Напечатать статистику за определённый месяц
             } else if (command == 2) {
                 System.out.println("Command 2");
+
+            //Изменить цель по количеству шагов в день
             } else if (command == 3) {
                 System.out.println("Введите новую цель по количеству шагов в день");
                 int newTargetSteps = scanner.nextInt();
@@ -30,6 +61,7 @@ public class calorie_counter {
             }
         }
     }
+
 
     private static void printMenu() {
         System.out.println("Что вы хотите сделать? ");
